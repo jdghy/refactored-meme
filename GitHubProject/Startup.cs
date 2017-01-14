ce5543b1cc70d1a1169d92f3e76eeff8f3f689d0
jdghy <jayantaduttaghy@gmail.com>
2017-01-15 01:36:47 +0530

    first commit

---
+++ GitHubProject/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(GitHubProject.Startup))]
+namespace GitHubProject
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
